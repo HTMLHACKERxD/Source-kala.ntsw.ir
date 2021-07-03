ef52f8e3d8f1b8c144e73e013a5b014e17fed9a9
HTMLHACKERxD <86883151+HTMLHACKERxD@users.noreply.github.com>
2021-07-03 18:51:09 +0430

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//---------loading------------------
+function showloading() {
+    //$(".loader-fadeout").css("visibility", "visible");
+    //$(".loader-fadeout").css("opacity", "0.8");
+    $("#loading").show();
+}
+function hideloading() {
+    //$(".loader-fadeout").css("visibility", "hidden");
+    $("#loading").hide();
+}
+//------------------------------------------
+
